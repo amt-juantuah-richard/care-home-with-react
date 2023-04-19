fa04a54f7a456815f4865bdb35c65ab24b1f0a49
mrbridge-dev <oseijuantuah@gmail.com>
2023-04-19 21:38:19 +0000

    marginal edits count

--- carehome/src/pages/ErrorPage.jsx
+++ carehome/src/pages/ErrorPage.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { useTitle } from 'react-use';
+import React, { useEffect } from 'react'
 // import styled from 'styled-components'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
@@ -7,10 +6,14 @@ import NoPage from '../components/NoPage';
 
 
 function ErrorPage() {
-  const location = window.location.pathname;
-  let pathSepereted = location.split("/");
-  const locString = pathSepereted[pathSepereted.length - 1];
-  useTitle(`Jo2Jos | ${locString? locString : ''} | Error`);
+  
+  useEffect(()=>{
+    const location = window.location.pathname;
+    const pathSepereted = location.split("/");
+    const locString = pathSepereted[pathSepereted.length - 1];
+    window.document.title = `Jo2Jos | ${locString? locString : ''} | Error`
+    }, []);
+
   return (
     <>
         <Navbar />
--- carehome/src/pages/Otherpages.jsx
+++ carehome/src/pages/Otherpages.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 // import styled from 'styled-components'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import ServicesDetail from '../components/ServiceDetail';
-import { useTitle } from 'react-use';
 
 
 function Otherpages() {
   
-  const location = window.location.pathname;
-  let pathSepereted = location.split("/");
-  const locString = pathSepereted[pathSepereted.length - 1];
-  useTitle(`Jo2Jos | ${locString}`);
+    useEffect(()=>{
+      const location = window.location.pathname;
+      const pathSepereted = location.split("/");
+      const locString = pathSepereted[pathSepereted.length - 1];
+      window.document.title = `Jo2Jos | ${locString? locString : ''} | Error`;
+    }, []);
 
   return (
     <>
